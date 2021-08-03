50f5e1a9ddc0f046238ed6373dd4ee50a397d565
Rastislav Wagner <rawagner@redhat.com>
2021-08-03 13:56:42 +0200

    Infra Env components work with k8s objects

--- src/components/Infra/InfraDetailsTab.tsx
+++ src/components/Infra/InfraDetailsTab.tsx
@@ -9,24 +9,10 @@ type InfraDetailsTabProps = {
   obj: CIM.InfraEnv;
 };
 
-const InfraDetailsTab: React.FC<InfraDetailsTabProps> = ({ obj }) => {
-  const labels = Object.keys(obj.status?.agentLabelSelector?.matchLabels || {}).map(
-    (k) => `${k}=${obj.status.agentLabelSelector.matchLabels[k]}`,
-  );
-  return (
-    <div className="co-m-pane__body">
-      <EnvironmentDetails
-        title="Environment details"
-        name={obj.metadata.name}
-        baseDomain="foo.com"
-        labels={labels}
-        location={obj.metadata.labels?.['assisted-installer-location'] || 'no location'}
-        creationDate={obj.metadata.creationTimestamp}
-        pullSecret="foo"
-        sshPublicKey="bar"
-      />
-    </div>
-  );
-};
+const InfraDetailsTab: React.FC<InfraDetailsTabProps> = ({ obj }) => (
+  <div className="co-m-pane__body">
+    <EnvironmentDetails infraEnv={obj} />
+  </div>
+);
 
 export default InfraDetailsTab;
