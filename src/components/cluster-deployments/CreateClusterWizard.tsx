3875cf4a7cffd6b926e2a945bd7292458afac7ee
Marek Libra <marek.libra@gmail.com>
2021-08-23 15:47:56 +0200

    MGMT-6485 Initial internationalization

--- src/components/cluster-deployments/CreateClusterWizard.tsx
+++ src/components/cluster-deployments/CreateClusterWizard.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { CIM } from 'openshift-assisted-ui-lib';
 import ClusterDeploymentWizard from './ClusterDeploymentWizard';
+import { getPreferredLang } from '../../utils';
 
 import './cluster-deployment.scss';
+
+const { I18N } = CIM;
 
 const CreateClusterWizard: React.FC<RouteComponentProps<{ ns: string }>> = ({ match, history }) => {
   const namespace = match?.params?.ns || 'assisted-installer';
 
-  return <ClusterDeploymentWizard history={history} namespace={namespace} />;
+  return (
+    <I18N preferredLang={getPreferredLang()}>
+      <ClusterDeploymentWizard history={history} namespace={namespace} />
+    </I18N>
+  );
 };
 
 export default CreateClusterWizard;
--- src/components/cluster-deployments/EditClusterWizard.tsx
+++ src/components/cluster-deployments/EditClusterWizard.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { CIM } from 'openshift-assisted-ui-lib';
 import ClusterDeploymentWizard from './ClusterDeploymentWizard';
+import { getPreferredLang } from '../../utils';
+
+const { I18N } = CIM;
 
 const EditClusterWizard: React.FC<RouteComponentProps<{ ns: string; name: string }>> = ({
   match,
@@ -10,11 +14,13 @@ const EditClusterWizard: React.FC<RouteComponentProps<{ ns: string; name: string
   const name = match.params?.name;
 
   return (
-    <ClusterDeploymentWizard
-      history={history}
-      namespace={namespace}
-      queriedClusterDeploymentName={name}
-    />
+    <I18N preferredLang={getPreferredLang()}>
+      <ClusterDeploymentWizard
+        history={history}
+        namespace={namespace}
+        queriedClusterDeploymentName={name}
+      />
+    </I18N>
   );
 };
 
